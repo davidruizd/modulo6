a6347b8b41cfcb3b15190f087f5e6d90909f7d57
David Ruiz <davidruizd@gmail.com>
2015-07-09 23:40:11 +0200

    Primera página y favicon

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
+/*var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// GET home page.
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+*/
+
+//Modulo 6
+var express = require('express');
+var router = express.Router();
+
+/*GET home page*/
+router.get('/',function(req, res){
+    res.render('index',{title: 'Quiz'});
+});
+
+module.exports = router;
+
+//FIN Modulo 6
